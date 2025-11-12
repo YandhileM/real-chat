c7078b806afddc5d4f3a77b89853a1e1b000f171
Yandhile <yandhilemaluleke@gmail.com>
2025-11-12 19:46:22 +0200

    feat: enhance Home component to fetch and display public and joined chat rooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,83 @@ import {
   EmptyMedia,
   EmptyTitle,
 } from "@/components/ui/empty";
-import {  MessageSquareIcon } from "lucide-react";
+import { getCurrentUser } from "@/services/supabase/lib/getCurrentUser";
+import { createAdminClient } from "@/services/supabase/server";
+import { MessageSquareIcon } from "lucide-react";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="conatainer mx-auto max-w-3xl px-4 py-8 space-y-8">
-      <Empty className="border border-dashed">
-        <EmptyHeader>
-          <EmptyMedia variant="icon">
-            <MessageSquareIcon />
-          </EmptyMedia>
-          <EmptyTitle>No Chat Rooms</EmptyTitle>
-          <EmptyDescription>
-            Create a new chat room to get started.
-          </EmptyDescription>
-        </EmptyHeader>
-        <EmptyContent>
-          <Button asChild>
-            <Link href="/rooms/new">New Chat Room</Link>
-          </Button>
-        </EmptyContent>
-      </Empty>
-    </div>
-  );
+export default async function Home() {
+  const user = await getCurrentUser();
+  if (user === null) {
+    redirect("/auth/login");
+  }
+
+  const [publicRooms, joinedRooms] = await Promise.all([
+    getPublicRooms(),
+    getJoinedRomms(user.id),
+  ]);
+
+  if (publicRooms.length === 0 && joinedRooms.length > 0) {
+    return (
+      <div className="conatainer mx-auto max-w-3xl px-4 py-8 space-y-8">
+        <Empty className="border border-dashed">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <MessageSquareIcon />
+            </EmptyMedia>
+            <EmptyTitle>No Chat Rooms</EmptyTitle>
+            <EmptyDescription>
+              Create a new chat room to get started.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="/rooms/new">New Chat Room</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    );
+  }
+}
+async function getPublicRooms() {
+  console.log("Fetching public rooms...");
+  const supabase = await createAdminClient();
+  const { data, error } = await supabase
+    .from("chat_room")
+    .select("id,name,chat_room_member (count)")
+    .eq("is_public", true)
+    .order("name", { ascending: true });
+
+  if (error) {
+    return [];
+  }
+
+  return data.map((room) => ({
+    id: room.id,
+    name: room.name,
+    memberCount: room.chat_room_member[0].count,
+  }));
+}
+
+async function getJoinedRomms(userId: string) {
+  const supabase = await createAdminClient();
+  const { data, error } = await supabase
+    .from("chat_room")
+    .select("id,name,chat_room_member (member_id)")
+    .eq("is_public", true)
+    .order("name", { ascending: true });
+
+  if (error) {
+    return [];
+  }
+
+  return data
+    .filter((room) => room.chat_room_member.some((u) => u.member_id === userId))
+    .map((room) => ({
+      id: room.id,
+      name: room.name,
+      memberCount: room.chat_room_member.length,
+    }));
 }
