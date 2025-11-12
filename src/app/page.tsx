1ff6e622f2e1b6a5cdffd4eede923673cbe98df7
Yandhile <yandhilemaluleke@gmail.com>
2025-11-12 20:12:27 +0200

    feat: implement RoomList and RoomCard components for displaying chat rooms with a create room button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,12 @@ import { createAdminClient } from "@/services/supabase/server";
 import { MessageSquareIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function Home() {
   const user = await getCurrentUser();
@@ -48,7 +54,69 @@ export default async function Home() {
       </div>
     );
   }
+  return (
+    <div className="container mx-auto px-4 py-8 space-y-8">
+      <RoomList title="Your Joined Rooms" rooms={joinedRooms} isJoined />
+      <RoomList
+        title="Public Rooms"
+        rooms={publicRooms.filter(
+          (room) => !joinedRooms.some((r) => r.id === room.id)
+        )}
+      />
+    </div>
+  );
 }
+
+function RoomList({
+  title,
+  rooms,
+  isJoined,
+}: {
+  title: string;
+  rooms: { id: string; name: string; memberCount: number }[];
+  isJoined?: boolean;
+}) {
+  if (rooms.length === 0) return null;
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between gap-2">
+        <h2 className="text-2xl">{title}</h2>
+        <Button asChild>
+          <Link href="/rooms/new">Create Room</Link>
+        </Button>
+      </div>
+      <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
+        {rooms.map((room) => (
+          <RoomCard {...room} key={room.id} isJoined={isJoined} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function RoomCard({
+  id,
+  name,
+  memberCount,
+  isJoined,
+}: {
+  id: string;
+  name: string;
+  memberCount: number;
+  isJoined?: boolean;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>
+          ({memberCount} member{memberCount !== 1 ? "s" : ""}) <br />
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
+
 async function getPublicRooms() {
   console.log("Fetching public rooms...");
   const supabase = await createAdminClient();
