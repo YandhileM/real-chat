144857e7ea96f68cbe5029ce7bbce493b5db4ddd
Yandhile <yandhilemaluleke@gmail.com>
2025-11-16 18:26:42 +0200

    feat: add createAdminClient function for admin client creation with Supabase

--- src/services/supabase/server.ts
+++ src/services/supabase/server.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { Database } from './types/database'
@@ -27,6 +29,20 @@ export async function createClient() {
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
+        },
+      },
+    }
+  )
+}
+
+export async function createAdminClient() {
+     return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SECRET_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return []
         },
       },
     }
