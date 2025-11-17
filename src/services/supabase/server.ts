9c8b61bdf061ec6ed80e5963e23c6236207f2ada
Yandhile <yandhilemaluleke@gmail.com>
2025-11-17 19:21:19 +0200

    refactor: clean up code formatting and remove unnecessary comments in server.ts

--- src/services/supabase/server.ts
+++ src/services/supabase/server.ts
@@ -1,15 +1,13 @@
-"use server"
-
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { Database } from './types/database'
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { Database } from "./types/database";
 
 /**
  * If using Fluid compute: Don't put this client in a global variable. Always create a new client within each
  * function when using it.
  */
 export async function createClient() {
-  const cookieStore = await cookies()
+  const cookieStore = await cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -17,13 +15,13 @@ export async function createClient() {
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
-            )
+            );
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -32,19 +30,19 @@ export async function createClient() {
         },
       },
     }
-  )
+  );
 }
 
-export async function createAdminClient() {
-     return createServerClient<Database>(
+export function createAdminClient() {
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SECRET_KEY!,
     {
       cookies: {
         getAll() {
-          return []
+          return [];
         },
       },
     }
-  )
+  );
 }
